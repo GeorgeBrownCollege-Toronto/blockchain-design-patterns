08fe12b033d8d3c61080b24f2805ba6ef7df7018
dhruvinparikh <imsilent93@gmail.com>
2021-12-14 19:58:38 -0500

    update tx

--- notes/2-lesson/examples/greeter-full-stack-dapp/pages/greet.tsx
+++ notes/2-lesson/examples/greeter-full-stack-dapp/pages/greet.tsx
@@ -6,7 +6,7 @@ import { Web3Provider } from "@ethersproject/providers";
 import { Contract } from "@ethersproject/contracts";
 import { abi as GreeterABI } from "../artifacts/contracts/Greeter.sol/Greeter.json";
 
-const address = "0x2d6ddceEAaa84E9fd422599BaF7184cDBF8Bd0DB";
+const address = "0x1d80895db5567d4752fc052eb272fa3270c7e363";
 
 export default function GreetPage() {
   const [greetingValue, setGreetingValue] = useState<string>("");
@@ -14,6 +14,7 @@ export default function GreetPage() {
   const [balance, setBalance] = useState<string>("");
   const [provider, setProvider] = useState<Web3Provider>();
   const [greeterInstance, setGreeterInstance] = useState<Contract>();
+  const [displayGreetingMessage, setDisplayGreetingMessage] = useState<string>("");
 
   async function handleInit() {
     if (typeof window !== "undefined") {
@@ -31,6 +32,7 @@ export default function GreetPage() {
         provider.getSigner()
       );
       setGreeterInstance(greeterInstance);
+      fetchGreeting()
     }
   }
 
@@ -40,6 +42,7 @@ export default function GreetPage() {
 
   async function fetchGreeting() {
     console.log(await greeterInstance?.greet());
+    setDisplayGreetingMessage(await greeterInstance?.greet())
   }
 
   async function setGreeting() {
@@ -54,7 +57,7 @@ export default function GreetPage() {
     <Layout title="Greet | Next.js + hardhat + ethers.js ">
       <h1>Greet</h1>
       <p>This is my first DApp</p>
-
+      <p>Greeting Message : {displayGreetingMessage}</p>
       <button onClick={fetchGreeting}>Fetch Greeting</button>
       <br />
       <br />
